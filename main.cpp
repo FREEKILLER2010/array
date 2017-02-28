7ccb29719c2bb01298c3ec75dfe048306e6d6d57
FREEKILLER201020 <htc.burchenko@gmail.com>
2017-02-28 13:05:50 +0300

    Get Delete Resete

--- main.cpp
+++ main.cpp
@@ -9,23 +9,48 @@ class Array {
 public:
   void **element;
 
-  int Length(){ // размера масива
+  int Length(){ // размер масива
     return count;
   }
 
-  void Push(void *data){ // добавить элемент в массив
+  void Push(void *data){ // добавить элемент в конец массива
     element=(void**)realloc(element,(count+1)*sizeof(void*));
     element[count]=(void*)malloc(sizeof(TYPE));
     memcpy(element[count],data,sizeof(TYPE));
     count++;
+
+  }
+  void Push(void *data, int position){ // добавить элемент в массив на nю позицию
+    if (position > count){
+      element=(void**)realloc(element,(position+1)*sizeof(void*));
+      count+=position-count+1;
+    }
+    element[position]=(void*)malloc(sizeof(TYPE));
+    memcpy(element[position],data,sizeof(TYPE));
   }
 
-//test
 
-  TYPE Pull(){
 
-    return *((TYPE*)element[count]);
+  TYPE Pull(){ // вытянуть последний элемент
+    TYPE tmp;
+    if ((element[count-1]) != 0){
+      tmp = *((TYPE*)element[count-1]);
+      free (element[count-1]);
+      return tmp;
+    }
+    return 0;
+  }
 
+  TYPE Pull(int position){ //вытянуть nй элемент
+    TYPE tmp;
+    if (position < count){
+      if ((element[position]) != 0){
+        tmp = *((TYPE*)element[position]);
+        free (element[position]);
+        return tmp;
+      }
+    }
+    return 0;
   }
 
 
@@ -36,9 +61,39 @@ public:
   }
 
 
-  void Get(){} // получить данные nго элемента (не вытягивая его)
-  void Delete(){} // удалить массив
-  void Reset(){} // очистить массив (возможно это дублирует Delete())
+  TYPE Get(){ // получить данные последнего элемента (не вытягивая его)
+    if ((element[count-1]) != 0){
+      return *((TYPE*)element[count-1]);
+    }
+    return 0;
+  }
+
+  TYPE Get(int position){ // получить данные nго элемента (не вытягивая его)
+    TYPE tmp;
+    if (position < count){
+      if ((element[position]) != 0){
+        return *((TYPE*)element[position]);
+      }
+    }
+    return 0;
+  }
+
+  void Delete(){ // удалить массив
+    for (int i=0;i<count;i++){
+      free(element[i]);
+    }
+    free(element);
+  }
+
+  TYPE Reset(){ // очистить массив (возможно это дублирует Delete())
+    for (int i=0;i<count;i++){
+      free(element[i]);
+    }
+    free(element);
+    element=(void**)malloc(sizeof(void*));
+    count=0;
+    return 0;
+  }
   void Swap(){} // Поменять местми nй и kй элемент
 
 
@@ -55,6 +110,8 @@ int main()
         Array<int> array;
         Array<float> array2;
         Array<char> array3;
+        Array<char> len;
+
 
         int test=1;
         float test2 = 3.5;
@@ -65,23 +122,33 @@ int main()
         array3.Create();
 
         array.Push(&test);
-        array2.Push(&test2);
+        array2.Push(&test2,0);
         array3.Push(&test3);
         test=2;
-        array.Push(&test);
+        array.Push(&test,5);
+        array.Push(&test,4);
 
 
 
-        cout << "test\n" << array.Pull()<<"\n";
-        cout << "test\n" << array2.Pull()<<"\n";
-        cout << "test\n" << array3.Pull()<<"\n";
+        cout << "test01\n" << array.Pull()<<"\n";
+        cout << "test02\n" << array2.Pull(0)<<"\n";
+        cout << "test03\n" << array3.Pull()<<"\n";
         test=2;
-        array.Push(&test);
-        cout << "test\n" << array.Pull()<<"\n";
+        array.Push(&test,2);
+        cout << "test04\n" << array3.Pull(3)<<"\n";
+
+        len.Create();
+        char a='a';
+        len.Push(&a);
+        len.Push(&a);
+        cout << "test05\n" << len.Length()<<"\n";
+        len.Delete();
+
 
 
         //float b=array.Pull(); // возврящает типа float
         //st c=array.Pull(); // возврящает типа st
+
 
 
 
