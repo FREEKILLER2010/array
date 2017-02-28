609523b8e1004b10f0bf18d62ff8ff3bcf4880f3
FREEKILLER201020 <htc.burchenko@gmail.com>
2017-02-28 17:17:28 +0300

    надо уходить от void**

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 //FIXME: free не правильно работате
+//FIXME: разобраться с удаление элементов
+//TODO: надо уходить от void** иначе некоторые данные могут не влезть...
 
 template< typename TYPE>
 class Array {
@@ -21,27 +23,28 @@ public:
     return count;
   }
 
-  void Push(void *data){ // добавить элемент в конец массива
+  void Push(TYPE data){ // добавить элемент в конец массива
     element=(void**)realloc(element,(count+1)*sizeof(void*));
-    element[count]=(void*)malloc(sizeof(TYPE));
-    memcpy(element[count],data,sizeof(TYPE));
+    element[count]= new TYPE;
+    memcpy(element[count],&data,sizeof(TYPE));
     count++;
 
   }
-  void Push(void *data, int position){ // добавить элемент в массив на nю позицию
+  void Push(TYPE data, int position){ // добавить элемент в массив на nю позицию
     if (position > count){
       element=(void**)realloc(element,(position+1)*sizeof(void*));
       count+=position-count+1;
     }
-    element[position]=(void*)malloc(sizeof(TYPE));
-    memcpy(element[position],data,sizeof(TYPE));
+    element[position]=malloc(sizeof(TYPE));
+    cout << "mem bef->" << element[position] << "\n";
+    memcpy(element[position],&data,sizeof(TYPE));
   }
 
   TYPE Pull(){ // вытянуть последний элемент
     TYPE tmp;
     if ((element[count-1]) != 0){
       tmp = *((TYPE*)element[count-1]);
-      free (element[count-1]);
+      element[count-1] = new(void*);
       return tmp;
     }
     return 0;
@@ -52,11 +55,12 @@ public:
       if (position < count){
         if ((element[position]) != 0){
           tmp = *((TYPE*)element[position]);
-          free (element[position]);
+          delete[] (element[position]);
+          cout << "mem aft->" << element[position] << "\n";
           return tmp;
         }
       }
-      return 0;
+      throw string("No Data");
     }
 
 /*
@@ -91,10 +95,12 @@ public:
     free(element);
   }
 
-  TYPE Reset(){ // очистить массив (возможно это дублирует Delete())
+  TYPE Reset(){ // очистить массив
     for (int i=0;i<count;i++){
       if (element[i]){
+        cout << "bef " << element[i] << " ! " << *((TYPE*)element[i]) << "\n";
         free(element[i]);
+        cout << "aft " << element[i] << " ! " << *((TYPE*)element[i]) << "\n";
       }
     }
     free(element);
@@ -102,7 +108,7 @@ public:
     count=0;
     return 0;
   }
-  TYPE Swap(int i, int j){ // Поменять местми nй и kй элемент
+  TYPE Swap(int i, int j){ // Поменять местми iй и jй элемент
     if ((element[i]) && (element[j])){
       void *tmp=malloc(sizeof(TYPE));
       memcpy(tmp,element[j],sizeof(TYPE));
@@ -121,24 +127,41 @@ public:
 
   };
 
+  struct test{
+    int i;
+    int j;
+  };
+
+typedef struct test test;
+
 int main()
 {
         Array<int> array;
+        Array<test> array2;
+        test to_push;
+        array2.Push(to_push);
         int test=1;
 
         //array.Create();
 
-        array.Push(&test);
-        test =2;
-        array.Push(&test);
+        array.Push(test,0);
+        test=2;
+        array.Push(test,1);
 
 
-        cout << "test01\n" << array.Pull(0)<<"\n";
-        cout << "test02\n" << array.Pull(1)<<"\n";
-        array.Swap(0,1);
-        cout << "test03\n" << array.Pull(0)<<"\n";
-        cout << "test04\n" << array.Pull(1)<<"\n";
+      //  cout << "test01\n" << array.Pull(0)<<"\n";
+        //cout << "test02\n" << array.Pull(0)<<"\n";
+        //array.Swap(0,1);
+        //cout << "test04\n" << array.Pull(4)<<"\n";
         array.Reset();
+        try
+     {
+       cout << "test03\n" << array.Pull(5)<<"\n";
+     }
+     catch(string std)
+     {
+          cout << std << "\n";
+     }
 
         return 0;
 }
